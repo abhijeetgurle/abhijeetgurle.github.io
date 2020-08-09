93ba695e9ddbaa6cf0fa1f9ae134f2bd374caa38
Abhijeet Gurle <abhijeet.gurle@quantiphi.com>
2020-08-09 21:05:55 +0530

    created build

---
+++ build/precache-manifest.aac3b1c78abb3543ee48162fc4782001.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ade64dd327f6021aef9e4018b7eae8b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24bedb09cf94b370188d",
+    "url": "/static/css/main.4e40a9fe.chunk.css"
+  },
+  {
+    "revision": "16c1513bbb3ef270abe2",
+    "url": "/static/js/2.d8b929c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d8b929c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "24bedb09cf94b370188d",
+    "url": "/static/js/main.aec5d263.chunk.js"
+  },
+  {
+    "revision": "28fc76dfc3e9feba5311",
+    "url": "/static/js/runtime-main.4677a4e5.js"
+  }
+]);
